d48f939c785ac183dc78e51ca461e8f45732baa8
jjang051tj <jjang051tj@daum.net>
2025-05-07 15:00:25 +0900

    include 분리

--- board/insert_process.php
+++ board/insert_process.php
@@ -1,4 +1,7 @@
 <?php
+
+    require_once "include/db.php";
+    
     $name = $_POST["name"];
     $subject = $_POST["subject"];
     $password = $_POST["password"];
@@ -6,29 +9,14 @@
     $regdate = date("Y-m-d H:i");
 
 
-    /*
-    echo "$name"."<br>";
-    echo "$password"."<br>";
-    echo "$subject"."<br>";
-    echo "$content"."<br>";
-    */
+ 
 
     
-
     
-    $servername = "localhost";
-    $username = "jjang051";
-    $password = "1234";
-    $db = "sample02";
-    $conn = mysqli_connect($servername,$username,$password,$db);
-    mysqli_begin_transaction($conn);
-    if(!$conn) {
-        die("연결오류 : ".mysqli_connect_error());
-    }
     $sql = "insert into board values (null,'$name','$subject','$password','$content','$regdate')";
     
     $result = mysqli_query($conn,$sql);
-    mysqli_commit($conn);
+    
     if($result) {
         echo "insert 성공";
     } else {
@@ -36,4 +24,5 @@
     }
     mysqli_close($conn);
     //echo "연결 성공";
+    echo "<script>location.href='list.php';</script>";
 ?>
