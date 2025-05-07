dba8dc68056a4beb1fcb2a6018c8a6cda356f12a
jjang051tj <jjang051tj@daum.net>
2025-05-07 14:16:52 +0900

    board insert

---
+++ board/insert.php
@@ -0,0 +1,34 @@
+<?php
+    require_once "include/header.php";
+?>
+    <h2 class="my-5">글쓰기</h2>
+    <form action="insert_process.php" method="post">
+        <div class="row mb-3">
+            <label for="name" class="col-sm-2 col-form-label">Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" name="name">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="password" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="subject" class="col-sm-2 col-form-label">Subject</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="subject" name="subject">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="content" class="col-sm-2 col-form-label">Content</label>
+            <div class="col-sm-10">
+                <textarea type="text" class="form-control" id="content" name="content"></textarea>
+            </div>
+        </div>
+        <div><button class="btn btn-primary">Sign in</button></div>
+    </form>
+<?php
+    require_once "include/footer.php";
+?>
--- board/insert_process.php
+++ board/insert_process.php
@@ -1,8 +1,21 @@
 <?php
+    $name = $_POST["name"];
+    $subject = $_POST["subject"];
+    $password = $_POST["password"];
+    $content = $_POST["content"];
+    $regdate = date("Y-m-d H:i");
 
-    $user_name = $_POST["userName"];
-    $address = $_POST["address"];
-    $tel = $_POST["tel"];
+
+    /*
+    echo "$name"."<br>";
+    echo "$password"."<br>";
+    echo "$subject"."<br>";
+    echo "$content"."<br>";
+    */
+
+    
+
+    
     $servername = "localhost";
     $username = "jjang051";
     $password = "1234";
@@ -12,7 +25,7 @@
     if(!$conn) {
         die("연결오류 : ".mysqli_connect_error());
     }
-    $sql = "insert into friend values (null,'$user_name','$tel','$address')";
+    $sql = "insert into board values (null,'$name','$subject','$password','$content','$regdate')";
     
     $result = mysqli_query($conn,$sql);
     mysqli_commit($conn);
